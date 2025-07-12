bd60c04e0a6e25f9de7b3dca91be91262c3de656
Wali <32612101+ibrahimwalle@users.noreply.github.com>
2025-07-13 01:17:52 +0300

    add new contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
 const ContactPage: React.FC = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "af29e1ff-c86b-4fd9-baa2-9be69a1c045b");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+        } else {
+        console.log("Error", data);
+        setResult(data.message);
+        }
+    };
+
     return (
         <div className="container mx-auto w-full">
             <div className="grid grid-cols-2 gap-16">
@@ -8,23 +34,23 @@ const ContactPage: React.FC = () => {
                     <div className="max-w-screen-md">
                         <h1 className="text-3xl pt-10 pb-8"><b>CONTACT</b></h1>
 
-                        <form className="space-y-8" action="mailto:ibrahimwalle20@gmail.com">
+                        <form className="space-y-8" onSubmit={onSubmit}>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                                <input type="email" id="email"
+                                <input type="email" id="email" name="email"
                                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-neutral-900 dark:border-neutral-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                     placeholder="example@gmail.com" required />
                             </div>
                             <div>
-                                <label htmlFor="subject"
-                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                                <input type="text" id="subject"
+                                <label htmlFor="Name"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                                <input type="text" id="name" name="name"
                                     className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-neutral-900 dark:border-neutral-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                    placeholder="Let me know how I can help you" required />
+                                    placeholder="John White" required />
                             </div>
                             <div className="sm:col-span-2">
                                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                                <textarea id="message" rows={6}
+                                <textarea id="message" rows={6} name="message"
                                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-neutral-900 dark:border-neutral-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                     placeholder="Leave a comment..." required></textarea>
                             </div>
@@ -33,6 +59,7 @@ const ContactPage: React.FC = () => {
                                 Send message
                             </button>
                         </form>
+                        {result && <div className="font-semibold">{result}</div>}
                     </div>
                 </section>
                 <div className="col-span-2 md:col-span-1 flex relative items-center">
